14d92d13e02a444b1d135f3486dd62025097b409
ABrandaoL <alexandre.brandao.lustosa@gmail.com>
2025-11-15 19:27:19 -0300

    Refactor xcAwake.pas for improved logic and parameters

--- xcAwake.pas
+++ xcAwake.pas
@@ -4,23 +4,66 @@
 // 00.00.02  20251115  1835
 //
 input
-  period(5);
-  useMedias(false);
+  period(14);
+  useMedias(true);
+  // types
+  // 1 - Trades
+  // 2 - Volume
+  // 3 - AskAndBid
+  // 4 - Quantity
+  // 5 - Synthetic
+  type(5);
+  tradesWeightOnSynthetic(1);
+  volumeWeightOnSynthetic(1);
+  askAndBidWeightOnSynthetic(2);
+ quantityWeightOnSynthetic(1);
 var
-  value,mtd : real;
-  color     : integer;
+  value : real;
+  color : integer;
 begin
+  value := 0;
   //
   // Calculation
   //
-  value := xcFnTrades();
+  if (type = 1) then
+    value := xcFnTrades()
+  else if (type = 2) then
+    value := xcFnVolume()
+  else if (type = 3) then
+    value := xcFnAskBidBalance()
+  else if (type = 4) then
+    value := xcFnQuantity()
+  else if (type = 5) then
+    begin
+      //Trades
+      if (xcFnTrades > 0) then
+        value := value + tradesWeightOnSynthetic
+      else if (xcFnTrades < 0) then
+        value := value - tradesWeightOnSynthetic;
+      //Volume
+      if (xcFnVolume > 0) then
+        value := value + volumeWeightOnSynthetic
+      else if (xcFnVolume < 0) then
+        value := value - volumeWeightOnSynthetic;
+      //Ask and Bid
+      if (xcFnAskBidBalance > 0) then
+        value := value + askAndBidWeightOnSynthetic
+      else if (xcFnAskBidBalance < 0) then
+        value := value - askAndBidWeightOnSynthetic;
+      //Quatity
+      if (xcFnQuantity > 0) then
+        value := value + quantityWeightOnSynthetic
+      else if (xcFnQuantity < 0) then
+        value := value - quantityWeightOnSynthetic;
+    end
+  else 
+    value := xcFnTrades();
   // Use media
   if (useMedias) then
     begin
       value := xcMSMA(period,2,value);
     end;
-  mtd := xcMSMA(7,2,value);
-  if (mtd > 0) then
+  if (value > 0) then
     color := clLime
   else 
     color := 255;
@@ -34,16 +77,16 @@ begin
   // Plotagem
   //
   PlotN(1,0);
-  PlotN(2,mtd);
+  PlotN(2,value);
   //
   // Force of direction
   //
-  if ((color = cllime) and (usemedias) and (mtd < mtd[1])) then
+  if ((color = cllime) and (usemedias) and (value < value[1])) then
     begin
       color := clGreen;
       SetPlotColor(2,color);
     end
-  else if ((color = 255) and (usemedias) and (mtd > mtd[1])) then
+  else if ((color = 255) and (usemedias) and (value > value[1])) then
     begin
       color := clMaroon;
       SetPlotColor(2,color);
